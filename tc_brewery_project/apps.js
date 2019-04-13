90f8b87e43bc6860d8b4025452fe67c4fcc6377b
Thad Stinson <bootytee@Thads-iMac.local>
2019-04-13 10:05:21 -0500

    success message for api

--- tc_brewery_project/apps.js
+++ tc_brewery_project/apps.js
@@ -1,1 +1,21 @@
 console.log($);
+$(() => {
+
+  $('form').on('submit', (event) => {
+    event.preventDefault();
+
+    const userInput = $('input[type="text"]').val();
+
+  $.ajax(
+    {
+      url:'https://sandbox-api.brewerydb.com/v2/?locations=locality=dallas/?key=b26731182c32ccfaa4c38d713ba3d33d/'
+    }
+  ).then(
+      (data)=>{
+        console.log(data);
+    },
+    ()=>{
+        console.log('bad request');
+    })
+  })
+});
