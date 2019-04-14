c3ee0b70a566d6c9293188427dd4bce798d017c6
Thad Stinson <bootytee@Thads-iMac.local>
2019-04-14 11:06:36 -0500

    added imgs and some css

--- tc_brewery_project/apps.js
+++ tc_brewery_project/apps.js
@@ -8,11 +8,19 @@ $(() => {
 
   $.ajax(
     {
-      url:'https://sandbox-api.brewerydb.com/v2/locations/?key=b26731182c32ccfaa4c38d713ba3d33d&region=texas'
+      url:'https://sandbox-api.brewerydb.com/v2/locations?key=b26731182c32ccfaa4c38d713ba3d33d&region=' + userInput,
     }
   ).then(
       (data)=>{
         console.log(data);
+        // $('#brewery-name').html(data.brewery.name);
+        // $('#website').html(data.website);
+        // $('#phone').html(data.phone);
+        // $('#streetAddress').html(data.streetAddress);
+        // $('#locality').html(data.locality);
+        // $('#region').html(data.region);
+        // $('#openToPublic').html(data.openToPublic);
+
     },
     ()=>{
         console.log('bad request');
