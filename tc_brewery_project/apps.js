3a2e4ff525699131e88bf420e1e8ff275f01068f
Thad Stinson <bootytee@Thads-iMac.local>
2019-04-14 17:52:20 -0500

    got all data to log on page

--- tc_brewery_project/apps.js
+++ tc_brewery_project/apps.js
@@ -1,4 +1,3 @@
-console.log($);
 
 $(() => {
 
@@ -14,8 +13,13 @@ $(() => {
   ).then(
       (data)=>{
           // console.log(data);
+          // const brewInfo = (name, website, phone, street, city, state, type) => {
+          //   for (let i = 0; i < 6; i++) {
+          //    ----what goes here?????
+          //   }
+          // }------loop atempt instead of hardcoding
         $('#name').html(data[0].name + data[1].name + data[2].name);
-        $('#website_url').html(data[0].website_urldata + [1].website_urldata + [2].website_url);
+        $('#website_url').html(data[0].website_url + data[1].website_url + data[2].website_url);
         $('#phone').html(data[0].phone + data[1].phone + data[2].phone);
         $('#street').html(data[0].street + data[1].street + data[2].street);
         $('#city').html(data[0].city + data[1].city + data[2].city);
@@ -26,5 +30,33 @@ $(() => {
     ()=>{
         console.log('bad request');
     })
-  })
+  })//------end of api call
+//--------beginning of modal po-up
+  // ===================
+  // GRABBING ELEMENTS
+  // ===================
+  const $openBtn = $('#openModal');
+  const $modal = $('#modal');
+  const $closeBtn = $('#close');
+
+  // ===================
+  // EVENT HANDLER
+  // ===================
+  // opens the modal
+  const openModal = () => {
+    $modal.css('display', 'block');
+  }
+  // closes the modal
+  const closeModal = () => {
+    $modal.css('display', 'none');
+  }
+
+  // ===================
+  // EVENT LISTENERS
+  // ===================
+  // about the game button
+  $openBtn.on('click', openModal);
+  // close button
+  $closeBtn.on('click', closeModal);
+
 });
