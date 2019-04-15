54a66bd2736096bdbc7d885fd0efb773f82e48c3
Thad Stinson <bootytee@Thads-iMac.local>
2019-04-15 15:26:49 -0500

    loop to add data to modal

--- tc_brewery_project/apps.js
+++ tc_brewery_project/apps.js
@@ -1,6 +1,6 @@
 
 $(() => {
-
+//need to mess with thi to submit info
   $('form').on('submit', (event) => {
     event.preventDefault();
 
@@ -8,7 +8,7 @@ $(() => {
 
   $.ajax(
     {
-      url:'https://api.openbrewerydb.org/breweries?per_page=3&by_type=micro&by_city=' + userInput,
+      url:'https://api.openbrewerydb.org/breweries?per_page=20&by_type=micro&by_city=' + userInput,
     }
   ).then(
       (data)=>{
@@ -18,44 +18,68 @@ $(() => {
           //    ----what goes here?????
           //   }
           // }------loop atempt instead of hardcoding
-        $('#name').html(data[0].name + data[1].name + data[2].name);
-        $('#website_url').html(data[0].website_url + data[1].website_url + data[2].website_url);
-        $('#phone').html(data[0].phone + data[1].phone + data[2].phone);
-        $('#street').html(data[0].street + data[1].street + data[2].street);
-        $('#city').html(data[0].city + data[1].city + data[2].city);
-        $('#state').html(data[0].state + data[1].state + data[2].state);
-        $('#brewery_type').html(data[0].brewery_type + data[1].brewery_type + data[2].brewery_type);
-          // console.log(data[0, 1]);
+        // -------------$('#name').html(data[0].name + data[1].name + data[2].name),
+        // $('#website_url').html(data[0].website_url + data[1].website_url + data[2].website_url),
+        // $('#phone').html(data[0].phone + data[1].phone + data[2].phone),
+        // $('#street').html(data[0].street + data[1].street + data[2].street),
+        // $('#city').html(data[0].city + data[1].city + data[2].city),
+        // $('#state').html(data[0].state + data[1].state + data[2].state),
+        // $('#brewery_type').html(data[0].brewery_type + data[1].brewery_type + data[2].brewery_type)];----------- this is good
+          // console.log(data);-------this is attempt
+          // $('#name').html(data.name),
+          // $('#website_url').html(data.website_url),
+          // $('#phone').html(data.phone),
+          // $('#street').html(data.street),
+          // $('#city').html(data.city),
+          // $('#state').html(data.state),
+          // $('#brewery_type').html(data.brewery_type)--------------
+        const popData = () => {  ///don't necessarily need this const
+          for (let i = 0; i < data.length; i++) {
+            const $infoContainer = $('<div>').addClass('info-container')
+            const $nameDiv = $('<div>').addClass('name').text(data[i].name)
+            const $webDiv = $('<div>').addClass('web').text(data[i].website_url)
+              $infoContainer.append($nameDiv)
+              $infoContainer.append($webDiv)
+              $('.modal-textbox').append($infoContainer)
+          }}
+          popData(); ///if take out const be sure to remove this function call and } above
     },
     ()=>{
         console.log('bad request');
     })
+
+   // }
   })//------end of api call
 //--------beginning of modal po-up
   // ===================
   // GRABBING ELEMENTS
   // ===================
-  const $openBtn = $('#openModal');
-  const $modal = $('#modal');
+  const $submit = $('#submit');
+  const $modal = $('.modal');
   const $closeBtn = $('#close');
+  const $modalText = $('.modal-textbox')
+  const $modalInfo = $('.modal-info')
 
   // ===================
   // EVENT HANDLER
   // ===================
   // opens the modal
   const openModal = () => {
-    $modal.css('display', 'block');
+    //need function to load data in to openModal
+    $modal.show();
   }
   // closes the modal
   const closeModal = () => {
-    $modal.css('display', 'none');
+    $modal.css('display', 'none')
+    $modalInfo.empty();  ///removes the header too :(
+    //$modalText.remove(); ////need to clear modal info .empty() or .remove() not sure this is working
   }
 
   // ===================
   // EVENT LISTENERS
   // ===================
   // about the game button
-  $openBtn.on('click', openModal);
+  $submit.on('click', openModal);
   // close button
   $closeBtn.on('click', closeModal);
 
