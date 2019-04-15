3ffad4bd2eaa8d105156e7155a7c6e9cb8ace608
Thad Stinson <bootytee@Thads-iMac.local>
2019-04-15 16:19:46 -0500

    all data populated and removed, styling modal

--- tc_brewery_project/apps.js
+++ tc_brewery_project/apps.js
@@ -12,37 +12,31 @@ $(() => {
     }
   ).then(
       (data)=>{
-          // console.log(data);
-          // const brewInfo = (name, website, phone, street, city, state, type) => {
-          //   for (let i = 0; i < 6; i++) {
-          //    ----what goes here?????
-          //   }
-          // }------loop atempt instead of hardcoding
-        // -------------$('#name').html(data[0].name + data[1].name + data[2].name),
-        // $('#website_url').html(data[0].website_url + data[1].website_url + data[2].website_url),
-        // $('#phone').html(data[0].phone + data[1].phone + data[2].phone),
-        // $('#street').html(data[0].street + data[1].street + data[2].street),
-        // $('#city').html(data[0].city + data[1].city + data[2].city),
-        // $('#state').html(data[0].state + data[1].state + data[2].state),
-        // $('#brewery_type').html(data[0].brewery_type + data[1].brewery_type + data[2].brewery_type)];----------- this is good
-          // console.log(data);-------this is attempt
-          // $('#name').html(data.name),
-          // $('#website_url').html(data.website_url),
-          // $('#phone').html(data.phone),
-          // $('#street').html(data.street),
-          // $('#city').html(data.city),
-          // $('#state').html(data.state),
-          // $('#brewery_type').html(data.brewery_type)--------------
-        const popData = () => {  ///don't necessarily need this const
+
+        const popData = () => {
+
           for (let i = 0; i < data.length; i++) {
             const $infoContainer = $('<div>').addClass('info-container')
             const $nameDiv = $('<div>').addClass('name').text(data[i].name)
             const $webDiv = $('<div>').addClass('web').text(data[i].website_url)
+            const $phoneDiv = $('<div>').addClass('phone').text(data[i].phone)
+            const $streetDiv = $('<div>').addClass('street').text(data[i].street)
+            const $cityDiv = $('<div>').addClass('city').text(data[i].city)
+            const $stateDiv = $('<div>').addClass('state').text(data[i].state)
+            const $brewery_typeDiv = $('<div>').addClass('brewery_type').text(data[i].brewery_type)
+
               $infoContainer.append($nameDiv)
               $infoContainer.append($webDiv)
-              $('.modal-textbox').append($infoContainer)
+              $infoContainer.append($phoneDiv)
+              $infoContainer.append($streetDiv)
+              $infoContainer.append($cityDiv)
+              $infoContainer.append($stateDiv)
+              $infoContainer.append($brewery_typeDiv)
+
+              $('.modal-info').append($infoContainer)
           }}
-          popData(); ///if take out const be sure to remove this function call and } above
+
+          popData();
     },
     ()=>{
         console.log('bad request');
@@ -50,7 +44,7 @@ $(() => {
 
    // }
   })//------end of api call
-//--------beginning of modal po-up
+//--------beginning of modal pop-up
   // ===================
   // GRABBING ELEMENTS
   // ===================
@@ -59,6 +53,7 @@ $(() => {
   const $closeBtn = $('#close');
   const $modalText = $('.modal-textbox')
   const $modalInfo = $('.modal-info')
+  const $infoContainer = $('.info-container') //had to define agian due to scope
 
   // ===================
   // EVENT HANDLER
@@ -68,11 +63,10 @@ $(() => {
     //need function to load data in to openModal
     $modal.show();
   }
-  // closes the modal
+  // closes the modal and clears $modalInfo divs
   const closeModal = () => {
     $modal.css('display', 'none')
-    $modalInfo.empty();  ///removes the header too :(
-    //$modalText.remove(); ////need to clear modal info .empty() or .remove() not sure this is working
+    $modalInfo.empty();
   }
 
   // ===================
